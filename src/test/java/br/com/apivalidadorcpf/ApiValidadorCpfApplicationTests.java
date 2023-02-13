77b62dd72f6999b5ff3dab1fe5ea62c0050c6f5b
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-02-13 07:29:59 -0300

    Projeto Criado

---
+++ src/test/java/br/com/apivalidadorcpf/ApiValidadorCpfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apivalidadorcpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiValidadorCpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
