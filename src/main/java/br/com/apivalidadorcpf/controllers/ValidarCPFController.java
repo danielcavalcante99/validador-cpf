a6a39439ce08835610dbe47e52d8efac885955c0
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-02-13 08:08:24 -0300

    Documentando API Swagger

--- src/main/java/br/com/apivalidadorcpf/controllers/ValidarCPFController.java
+++ src/main/java/br/com/apivalidadorcpf/controllers/ValidarCPFController.java
@@ -2,12 +2,16 @@ package br.com.apivalidadorcpf.controllers;
 
 import br.com.apivalidadorcpf.models.Status;
 import br.com.apivalidadorcpf.services.ValidaCPFService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Validação")
 @RestController
 public class ValidarCPFController {
 
@@ -20,6 +24,9 @@ public class ValidarCPFController {
     }
 
     @GetMapping("/users/{cpf}")
+    @Operation(summary = "Consultar todos os produtos")
+    @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso")
+    @ApiResponse(responseCode = "404", description = "CPF inválido")
     public ResponseEntity<Status> validarCPF(@PathVariable String cpf) {
         boolean isValid = this.service.validar(cpf);
         if (isValid) {
